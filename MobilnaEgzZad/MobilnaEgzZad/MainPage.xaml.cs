5cabc5ef8aadcb3c393e106d862ad44b1a3f5f9b
PL <labuzpat05@gmail.com>
2025-01-09 22:49:17 +0100

    d

--- MobilnaEgzZad/MobilnaEgzZad/MainPage.xaml.cs
+++ MobilnaEgzZad/MobilnaEgzZad/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace MobilnaEgzZad
             InitializeComponent();
         }
 
-        private void CheckPrice(object sender, EventArgs e)
+        /*private void CheckPrice(object sender, EventArgs e)
         {
             if (washing.IsChecked)
             {
@@ -36,13 +36,48 @@ namespace MobilnaEgzZad
                 Price *= 1.3;
             }
             price.Text = Price.ToString();
-        }
+        }*/
+
 
         private void Accept(object sender, EventArgs e)
         {
-            if (washing.IsChecked || vaccuming.IsChecked && price.Text != "")
+            if (washing.IsChecked || vaccuming.IsChecked)
             {
-                DisplayAlert("Zamówienie zaakceptowane!", "Twoje zamówienie: " + service + "zostało zaakceptowane " + "\n Cena: " + Price + " zł", "OK");
+                DisplayAlert("Zamówienie zaakceptowane!", "Twoje zamówienie: " + service + " zostało zaakceptowane" + "\nCena: " + Price + " zł", "OK");
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Wybierz usługę", "OK");
+            }
+        }
+
+        private void CheckSelection(object sender, CheckedChangedEventArgs e)
+        {
+            if (washing.IsChecked)
+            {
+                Price = 50;
+                service = "Pranie";
+                image.Source = "pralka.jpg";
+            }
+            if (vaccuming.IsChecked)
+            {
+                Price = 70;
+                service = "Odkurzanie";
+                image.Source = "odkurzacz.jpg";
+            }
+            price.Text = Price.ToString();
+        }
+
+        private void CheckCheckbox(object sender, CheckedChangedEventArgs e)
+        {
+            if (fasterService.IsChecked)
+            {
+                price.Text = (1.3 * Price).ToString();
+            }
+            else
+            {
+                price.Text = Price.ToString();
+
             }
         }
     }
